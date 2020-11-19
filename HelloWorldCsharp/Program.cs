8bd3fea9448349270faa1db55529f824011b2840
ShairahJoyceMurillo <72656585+ShairahJoyceMurillo@users.noreply.github.com>
2020-11-19 11:53:20 +0800

    init hello world

---
+++ HelloWorldCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 11/19/2020
+ * Time: 11:52 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace HelloWorldCsharp
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
