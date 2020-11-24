a746fbe233177f739c8c50922ec374cb70c1dca5
ShairahJoyceMurillo <72656585+ShairahJoyceMurillo@users.noreply.github.com>
2020-11-24 16:39:15 +0800

    added webapi

---
+++ sharpdevelopwebapi-sdwebapi-vuejs/SharpDevelopWebApi/Controllers/SongController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Mvc;
+using SharpDevelopWebApi.Models;
+
+namespace SharpDevelopWebApi.Controllers
+{
+	/// <summary>
+	/// Description of SongController.
+	/// </summary>
+	public class SongController : ApiController
+	{
+		SDWebApiDbContext _db = new SDWebApiDbContext();
+		
+		[HttpGet]
+		public IHttpActionResult GetAll()
+		{		
+			List<Song> songs = _db.Songs.ToList();
+			return Ok(songs);
+		}
+		
+		[HttpGet] 
+		public IHttpActionResult Get(int Id)
+		{
+			var song = _db.Songs.Find(Id);
+			if(song != null)
+				return Ok(song);
+			else
+				return BadRequest("Song not found");
+			
+		}
+		
+		[HttpPost]
+		public IHttpActionResult Create([FromBody]Song song)
+		{			
+			_db.Songs.Add(song);
+			_db.SaveChanges();
+			return Ok(song.Id);
+		}
+		
+		[HttpPut]
+		public IHttpActionResult Update([FromBody]Song updatedSong)
+		{
+			var song = _db.Songs.Find(updatedSong.Id);
+			if(song != null)
+			{
+				song.Artist = updatedSong.Artist;
+				song.Title = updatedSong.Title;
+				song.Genre = updatedSong.Genre;
+				_db.Entry(song).State = EntityState.Modified;
+				_db.SaveChanges();
+				
+				return Ok(song);			
+			}
+			else
+			{
+				return BadRequest("Song not found");
+			}
+
+		}
+		
+	
+		[HttpDelete]
+		public IHttpActionResult Delete(int Id)
+		{
+			var songToDelete = _db.Songs.Find(Id);
+			if(songToDelete != null)
+			{
+				_db.Songs.Remove(songToDelete);
+				_db.SaveChanges();
+				return Ok("Successfully deleted");
+			}
+			else
+			{
+				return BadRequest("Song not found");
+			}
+		}
+	}
+}
